a4544380fe72f10abddc7341c8c956b9dbc38782
zhanjunjun <604021005@qq.com>
2019-12-17 15:12:33 +0800

    添加用户信息查看的Ui

--- app/src/main/java/com/zoe/diary/ui/activity/DiaryUserActivity.java
+++ app/src/main/java/com/zoe/diary/ui/activity/DiaryUserActivity.java
@@ -1,17 +1,46 @@
 package com.zoe.diary.ui.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.zoe.diary.R;
 import com.zoe.diary.ui.activity.base.BaseActivity;
 
+import butterknife.BindView;
+
 public class DiaryUserActivity extends BaseActivity {
+
+    @BindView(R.id.iv_user_icon)
+    ImageView ivUserIcon;
+
+    @BindView(R.id.tv_user_id)
+    TextView tvUserId;
+
+    @BindView(R.id.tv_user_nick)
+    TextView tvUserNick;
+
+    @BindView(R.id.tv_bind_qq)
+    TextView tvBindQQ;
+
+    @BindView(R.id.tv_bind_wechat)
+    TextView tvBindWeChat;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initData();
+        initView();
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+
     }
 
     @Override
